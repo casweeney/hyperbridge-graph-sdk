426bd7824a2a6714ab833edc397346d7caa97ee2
casweeney <casweeno2000@gmail.com>
2024-03-09 23:42:48 +0100

    UPDATE: implemented relayer post request count

--- src/graphQl/relayerPostRequestHandledCount.ts
+++ src/graphQl/relayerPostRequestHandledCount.ts
@@ -1,1 +1,28 @@
-export async function handleRelayerPostRequestHandledCount() {}
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client'
+import { SUBGRAPH_API_URL } from '../constants'
+import { QueryOptions } from '../types'
+
+const client = new ApolloClient({
+  uri: SUBGRAPH_API_URL,
+  cache: new InMemoryCache(),
+})
+
+export async function handleRelayerPostRequestHandledCount(relayerAddress: string) {
+  const operationName = QueryOptions.RelayerPostRequestHandledCount
+
+  const response = await client.query({
+    query: gql`
+      query ${operationName}($relayerId: ID!) {
+        relayerPostRequestHandledCount(id: $relayerId) {
+          id
+          value
+        }
+      }
+    `,
+    variables: {
+      relayerId: relayerAddress,
+    },
+  })
+
+  return response
+}
