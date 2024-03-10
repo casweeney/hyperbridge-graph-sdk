dd26b11141cf91e30179971e3c207efacdbeb6a9
casweeney <casweeno2000@gmail.com>
2024-03-10 18:56:17 +0100

    CLEAN_CODE: remove repetition of client into a constants.ts file

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 // all constant would go into this folder
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+import { ApolloClient, InMemoryCache } from '@apollo/client'
 export const SUBGRAPH_API_URL = 'https://api.studio.thegraph.com/query/66742/hyperbridgeupdated/v0.0.3'
 
 export const client = new ApolloClient({
