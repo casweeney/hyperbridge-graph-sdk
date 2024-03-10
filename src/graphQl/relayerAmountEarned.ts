7db41149fedd539c523b3113d18b3cd067c52d44
casweeney <casweeno2000@gmail.com>
2024-03-10 07:28:41 +0100

    UPDATE: implemented relayer amount earned query

--- src/graphQl/relayerAmountEarned.ts
+++ src/graphQl/relayerAmountEarned.ts
@@ -1,1 +1,33 @@
-export async function handleRelayerAmountEarned() {}
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client'
+import { SUBGRAPH_API_URL } from '../constants'
+import { QueryOptions } from '../types'
+
+const client = new ApolloClient({
+  uri: SUBGRAPH_API_URL,
+  cache: new InMemoryCache(),
+})
+
+export async function handleRelayerAmountEarned(hostAddress: string, relayerAddress: string) {
+  const operationName = QueryOptions.RelayerPostRequestHandledCount
+
+  const response = await client.query({
+    query: gql`
+        query ${operationName}($hostAddress: String!, $relayerAddress: String!) {
+          transferPairTotals(
+            where: {from: $hostAddress, to: $relayerAddress}
+          ) {
+            from
+            id
+            to
+            totalAmount
+          }
+        }
+      `,
+    variables: {
+      hostAddress: hostAddress,
+      relayerAddress: relayerAddress,
+    },
+  })
+
+  return response
+}
