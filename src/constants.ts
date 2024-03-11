1c44c52e0cb5eb7fd766045631e6b6b71acc3408
casweeney <casweeno2000@gmail.com>
2024-03-11 21:31:27 +0100

    REFACTORING: constant updated

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 // all constant would go into this folder
-export const SUBGRAPH_API_URL = 'https://api.studio.thegraph.com/query/66742/hyperbridgeupdated/v0.0.3'
+export const SEPOLIA_SUBGRAPH_API_URL = 'https://api.studio.thegraph.com/query/66742/hyperbridgeupdated/v0.0.3'
